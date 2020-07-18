be2de9852e941549609d3c08d1debfc3522fe879
Eli Fatsi <eli.fatsi@viget.com>
2020-07-18 17:11:38 -0400

    Pretending like I have a real intersection

--- src/index.js
+++ src/index.js
@@ -12,25 +12,36 @@ const app = new PIXI.Application({
 })
 document.body.appendChild(app.view)
 
-// Add a fork of roads
+const intersectionContainer = new PIXI.Container()
+intersectionContainer.sortableChildren = true
+app.stage.addChild(intersectionContainer)
+
 const road = new Road(50, 200, 170, 200)
 app.stage.addChild(road.graphics)
+const intersection = new Road(170, 200, 230, 200, true, true)
+intersectionContainer.addChild(intersection.graphics)
 const road2 = new Road(230, 200, 350, 200)
 app.stage.addChild(road2.graphics)
 
-const intersection = new Road(170, 200, 230, 200, true)
-app.stage.addChild(intersection.graphics)
+const road3 = new Road(200, 50, 200, 170)
+app.stage.addChild(road3.graphics)
+const intersection2 = new Road(200, 170, 200, 230, true, false)
+intersectionContainer.addChild(intersection2.graphics)
+const road4 = new Road(200, 230, 200, 350)
+app.stage.addChild(road4.graphics)
 
-let intersections = [intersection]
+let intersections = [intersection, intersection2]
 
 // Click to add cars
 app.stage.interactive = true
 app.renderer.plugins.interaction.on('pointerdown', onPointerDown)
 let cars = []
 function onPointerDown() {
-  const car = new Car([road, intersection, road2])
-  app.stage.addChild(car.graphics)
-  cars.push(car)
+  const car1 = new Car([road, intersection, road2])
+  const car2 = new Car([road3, intersection2, road4])
+  app.stage.addChild(car1.graphics)
+  app.stage.addChild(car2.graphics)
+  cars.push(car1, car2)
 }
 
 // Make cars move
@@ -38,8 +49,11 @@ app.ticker.add(delta => {
   cars.forEach(c => {
     c.tick(delta)
   })
+
+  // Render closed intersections first so the red is on the bottom
+  debugger
   intersections.forEach(i => {
-    i.tick(delta)
+    i.tick()
   })
 
   // Remove cars that have made it to their destination
--- src/models/road.js
+++ src/models/road.js
@@ -1,14 +1,14 @@
 import * as PIXI from 'pixi.js'
 
 export default class Road {
-  constructor(startX, startY, endX, endY, intersection) {
+  constructor(startX, startY, endX, endY, intersection, open) {
     this.startX = startX
     this.startY = startY
     this.endX = endX
     this.endY = endY
     this.intersection = intersection
     if (this.intersection) {
-      this.open = true
+      this.open = open
 
       setInterval(() => {
         this.open = !this.open
@@ -39,13 +39,15 @@ export default class Road {
 
   // intersections only
   // optimization available by not repainint every tick
-  tick(delta) {
+  tick() {
     this.graphics.clear()
 
     if (this.open) {
-      this.graphics.lineStyle(2, 0x49a43e)
+      this.graphics.lineStyle(4, 0x49a43e)
+      this.graphics.zIndex = 1
     } else {
-      this.graphics.lineStyle(2, 0xd43b3b)
+      this.graphics.lineStyle(4, 0xd43b3b)
+      this.graphics.zIndex = -1
     }
 
     this.render()
